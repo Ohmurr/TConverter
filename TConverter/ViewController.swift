ba4e2a97a0f0297f9ffb7773796dbe6f32a11039
Ohmurr <93642244+Ohmurr@users.noreply.github.com>
2021-12-21 00:08:44 +0300

    Add files via upload

---
+++ TConverter/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Anna on 19.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var cenliusLable: UILabel!
+    @IBOutlet weak var fahrenheLable: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+   }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let temperatureCelsius = Int(round(sender.value))
+        cenliusLable.text = "\(temperatureCelsius)ºC"
+        let fahrenheitTemperature = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheLable.text = "\(fahrenheitTemperature)ºF"
+    }
+}
+
+
+
